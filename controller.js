7dd8c8f2643b5d55ef29d3b1285d5b9817590ae3
Alvin <alvinsujono10@outlook.com>
2020-05-17 15:34:59 +0700

    ubah service

--- controller.js
+++ controller.js
@@ -255,4 +255,24 @@ exports.ubahmontir = function (req, res) {
                         response.ok("Level Berhasil Diubah", res);
                     }
                 });
+        };//ubah servis
+        exports.ubahservis = function (req, res) {
+            var id_servis = req.body.id_servis;
+            var tgl_servis = req.body.tgl_servis;
+            var id_user = req.body.id_user;
+            var id_montir = req.body.id_montir;
+            var jumlah_sparepart = req.body.jumlah_sparepart;
+            var id_sparepart = req.body.id_sparepart;
+        
+            connection.query('UPDATE t_servis SET tgl_servis=?,id_user=?,id_montir=?,jumlah_sparepart=?,id_sparepart=? WHERE id_servis=?',
+            [tgl_servis,id_user,id_montir,jumlah_sparepart,id_sparepart,id_servis],
+        
+                function (error, rows, fields) {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        response.ok("Data Servis Berhasil Diubah", res);
+                    }
+                });
         };
+        
