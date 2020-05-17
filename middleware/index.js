424b73d12a015c9dab127ab216547e83c5c23c4e
Alvin <alvinsujono10@outlook.com>
2020-05-17 15:38:14 +0700

    menambah put delet get pada index

--- middleware/index.js
+++ middleware/index.js
@@ -15,3 +15,20 @@ router.post('/user/admin/input/sparepart', verifikasi.verifikasi1(), control.tam
 router.post('/user/admin/input/user', verifikasi.verifikasi1(), control.tambahuser);
 router.post('/user/admin/input/level', verifikasi.verifikasi1(), control.tambahlevel);
 router.post('/user/admin/input/servis', verifikasi.verifikasi1(), control.tambahservis);
+//alamat yang perlu otorisasi
+router.get('/user/pelanggan/tampil/biayaservis', verifikasi.verifikasi2(), control.tampilgroupsparepart);
+
+
+router.put('/user/admin/ubah/montir', verifikasi.verifikasi1(), control.ubahmontir);
+router.put('/user/admin/ubah/sparepart', verifikasi.verifikasi1(), control.ubahsparepart);
+router.put('/user/admin/ubah/user', verifikasi.verifikasi1(), control.ubahuser);
+router.put('/user/admin/ubah/level', verifikasi.verifikasi1(), control.ubahlevel);
+router.put('/user/admin/ubah/servis', verifikasi.verifikasi1(), control.ubahservis);
+
+router.delete('/user/admin/hapus/montir', verifikasi.verifikasi1(), control.hapusmontir);
+router.delete('/user/admin/hapus/sparepart', verifikasi.verifikasi1(), control.hapussparepart);
+router.delete('/user/admin/hapus/user', verifikasi.verifikasi1(), control.hapususer);
+router.delete('/user/admin/hapus/level', verifikasi.verifikasi1(), control.hapuslevel);
+router.delete('/user/admin/hapus/servis', verifikasi.verifikasi1(), control.hapusservis);
+
+module.exports = router;
